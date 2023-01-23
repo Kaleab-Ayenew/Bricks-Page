4a20925856f163a9bb1d24931247ad11f29469f3
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2023-01-24 01:14:37 +0300

    Added full functionality to the Nav Bar, gave it a nice look

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,5 @@
 let btn = document.getElementsByClassName("header__menu-btn")[0];
 let nav = document.getElementsByClassName("header__bottom-nav")[0];
-btn.addEventListener('click',()=>{
+btn.addEventListener("click", () => {
   nav.classList.toggle("collapse");
-})
+});
