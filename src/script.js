b6f3cc4e60e0799d8e7bfc2876dca30cb6602f23
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2023-01-25 13:45:21 +0300

    removed src and added favicon

--- src/script.js
+++
@@ -1,5 +0,0 @@
-let btn = document.getElementsByClassName("header__menu-btn")[0];
-let nav = document.getElementsByClassName("header__bottom-nav")[0];
-btn.addEventListener('click',()=>{
-  nav.classList.toggle("collapse");
-})
